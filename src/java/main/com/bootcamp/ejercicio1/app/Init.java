1d932a542832c7efcc7af501e7c657040000756a
ArCiGo <armandocifuentes_1@hotmail.com>
2019-07-25 16:11:33 -0500

    Update of defender() and ataque() methods

--- src/java/main/com/bootcamp/ejercicio1/app/Init.java
+++ src/java/main/com/bootcamp/ejercicio1/app/Init.java
@@ -3,12 +3,13 @@ package com.bootcamp.ejercicio1;
 import com.bootcamp.ejercicio1.core.characters.Elfo;
 import com.bootcamp.ejercicio1.core.characters.Enano;
 import com.bootcamp.ejercicio1.core.characters.Humano;
+import com.bootcamp.ejercicio1.core.characters.Personaje;
 
 public class Init {
     public static void main(String args[]) {
         System.out.println("Bienvenido a mi juego");
 
-        Humano humano1 = new Humano("Armando");
+        /*Humano humano1 = new Humano("Armando");
         Humano humano2 = new Humano("Jesús");
 
         Elfo elfo1 = new Elfo("Blanca");
@@ -20,6 +21,19 @@ public class Init {
         //humano1.nombreHumano = "Eduardo";
 
         System.out.println("El objeto humano1 se llama: " + humano1.getHumanName());
-        System.out.println("Asignando el nombre desde la variable nombreHumano: " + humano1.nombreHumano);
+        System.out.println("Asignando el nombre desde la variable nombreHumano: " + humano1.nombreHumano);*/
+
+        /***************** */
+
+        Personaje personaje1 = new Personaje(6, 6, 6, 6, 6, "Armando", 27, 'H');
+        Personaje personaje2 = new Personaje(6, 4, 6, 4, 9, "Eduardo", 18, 'H');
+        Personaje personaje3 = new Personaje(6, 6, 5, 8, 6, "Paola", 25, 'M');
+
+        personaje1.ataque(personaje2, personaje3);
+        personaje2.ataque(personaje1, personaje3);
+        personaje3.ataque(personaje2, personaje1);
+        personaje1.ataque(personaje2);
+
+        System.out.println("Finalizó el juego");
     }
 }
--- src/java/main/com/bootcamp/ejercicio1/core/characters/Personaje.java
+++ src/java/main/com/bootcamp/ejercicio1/core/characters/Personaje.java
@@ -12,7 +12,57 @@ public class Personaje {
     public char sexo;
     public static final int NUM_VIDAS_DEFAULT = 3;
 
+    public Personaje(int vida, int agilidad, int defensa, int magia, int ataque, String nombre, int edad, char sexo) {
+        this.vida = vida;
+        this.agilidad = agilidad;
+        this.defensa = defensa;
+        this.magia = magia;
+        this.ataque = ataque;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+    }
+
+    public int ataque() {
+        int ataqueTotal = this.ataque + this.magia;
+
+        return ataqueTotal;
+    }
+
     public Personaje[] ataque(Personaje... personaje) {
+        int ataqueTotal = this.ataque();
+
+        for(Personaje item : personaje) {
+            if(item.vida <= 0) {
+                System.out.println("El personaje: " + item.nombre + " está muerto");
+            } else {
+                item.defender(ataqueTotal, this);
+
+                System.out.println("Se atacó al personaje: " + item.nombre);
+                System.out.println("Vida restante: " + item.vida);
+            }
+        }
+
+        return personaje;
+    }
+
+    public void defender(int ataqueTotal, Personaje personajeA) {
+        int defensaTotal = this.defensa + this.agilidad;
+
+        this.vida = this.vida - (ataqueTotal - defensaTotal);
+
+        if(this.vida <= 0) {
+            System.out.println("El personaje: " + this.nombre + " murió");
+        }
+    }
+
+    public String toString() {
+        return "Vida: " + vida + ", Agilidad: " + agilidad + ", Defensa" + defensa + ", Magia: " + magia +
+        ", Ataque: " + ataque + ", Nombre: " + nombre + ", Edad: " + edad + ", Sexo: " + sexo; 
+    }
+
+    // first approach approved by Diego
+    /*public Personaje[] ataque(Personaje... personaje) {
         for(int i = 0; i < personaje.length; i ++) {
             personaje[i].defender(this);
         }
@@ -24,5 +74,5 @@ public class Personaje {
         int defensaTotal = this.defensa + this.agilidad;
 
         this.vida = this.vida - defensaTotal - personajeA.ataque + personajeA.magia ;
-    }
+    }*/
 }
